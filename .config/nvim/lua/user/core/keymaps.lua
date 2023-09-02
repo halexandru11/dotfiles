57d5fea76e03eea794a08b5faac767298b48bc1a
halexandru11 <77728536+halexandru11@users.noreply.github.com>
2023-09-02 10:02:26 +0300

    Added window maximizer in nvim configuration

--- .config/nvim/lua/user/core/keymaps.lua
+++ .config/nvim/lua/user/core/keymaps.lua
@@ -2,58 +2,59 @@
 vim.g.mapleader = " "
 
 local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 
 ---------------------
 -- General keymaps
 ---------------------
 
 -- use jk to exit insert mode
-keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
-keymap.set("i", "jl", "<Esc>", { noremap = true, silent = true })
-keymap.set("i", "kl", "<Esc>", { noremap = true, silent = true })
-keymap.set("i", "JK", "<Esc>", { noremap = true, silent = true })
-keymap.set("i", "JL", "<Esc>", { noremap = true, silent = true })
-keymap.set("i", "KL", "<Esc>", { noremap = true, silent = true })
+keymap.set("i", "jk", "<Esc>", opts)
+keymap.set("i", "jl", "<Esc>", opts)
+keymap.set("i", "kl", "<Esc>", opts)
+keymap.set("i", "JK", "<Esc>", opts)
+keymap.set("i", "JL", "<Esc>", opts)
+keymap.set("i", "KL", "<Esc>", opts)
 
 -- move lines up and down
-keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })  -- move line up
-keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })  -- move line down
-keymap.set("v", "p", '"_dP', { noremap = true, silent = true })                -- paste without yanking
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts) -- move line up
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts) -- move line down
+keymap.set("v", "p", '"_dP', opts) -- paste without yanking
 
-keymap.set("n", "<C-d>", "<c-d>zz", { noremap = true, silent = true })         -- move down half page
-keymap.set("n", "<C-u>", "<c-u>zz", { noremap = true, silent = true })         -- move up half page
-keymap.set("n", "n", "nzzzv", { noremap = true, silent = true })               -- keep cursor centered when searching
-keymap.set("n", "N", "Nzzzv", { noremap = true, silent = true })               -- keep cursor centered when searching
+keymap.set("n", "<C-d>", "<c-d>zz", opts) -- move down half page
+keymap.set("n", "<C-u>", "<c-u>zz", opts) -- move up half page
+keymap.set("n", "n", "nzzzv", opts) -- keep cursor centered when searching
+keymap.set("n", "N", "Nzzzv", opts) -- keep cursor centered when searching
 
-keymap.set("n", "<leader>r", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")  -- replace word under cursor
-keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>")  -- make file executable
+keymap.set("n", "<leader>r", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>") -- replace word under cursor
+keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>") -- make file executable
 
 -- clear search highlights
-keymap.set("n", "<leader>nh", ":noh<CR>", { noremap = true, silent = true })
+keymap.set("n", "<leader>nh", ":noh<CR>", opts)
 
 -- delete single character without yanking
-keymap.set("n", "x", '"_x', { noremap = true, silent = true })
+keymap.set("n", "x", '"_x', opts)
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { noremap = true, silent = true })  -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { noremap = true, silent = true })  -- split window horizontally
-keymap.set("n", "<leader>so", "<C-w>o", { noremap = true, silent = true })  -- close all windows except current
-keymap.set("n", "<leader>se", "<C-w>=", { noremap = true, silent = true })  -- equalize window sizes
-keymap.set("n", "<leader>sx", ":close<CR>", { noremap = true, silent = true })  -- close current window
+keymap.set("n", "<leader>sv", "<C-w>v", opts) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", opts) -- split window horizontally
+keymap.set("n", "<leader>so", "<C-w>o", opts) -- close all windows except current
+keymap.set("n", "<leader>se", "<C-w>=", opts) -- equalize window sizes
+keymap.set("n", "<leader>sx", ":close<CR>", opts) -- close current window
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) -- maximize current window
 
 -- resize windows
-keymap.set("n", "<C-Up>", ":resize +2<CR>", { noremap = true, silent = true })  -- resize window up
-keymap.set("n", "<C-Down>", ":resize -2<CR>", { noremap = true, silent = true })  -- resize window down
-keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { noremap = true, silent = true })  -- resize window left
-keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { noremap = true, silent = true })  -- resize window right
+keymap.set("n", "<C-Up>", ":resize +2<CR>", opts) -- resize window up
+keymap.set("n", "<C-Down>", ":resize -2<CR>", opts) -- resize window down
+keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts) -- resize window left
+keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts) -- resize window right
 
 -- stay in indent mode
-keymap.set("v", "<", "<gv", { noremap = true, silent = true })  -- indent left
-keymap.set("v", ">", ">gv", { noremap = true, silent = true })  -- indent right
+keymap.set("v", "<", "<gv", opts) -- indent left
+keymap.set("v", ">", ">gv", opts) -- indent right
 
 -- show file tree
-keymap.set("n", "<leader>e", ":Ex<CR>", { noremap = true, silent = true })
-
+keymap.set("n", "<leader>e", ":Ex<CR>", opts)
 
 ---------------------
 -- Plugin keymaps
--- .config/nvim/lua/user/plugins-setup.lua
+++ .config/nvim/lua/user/plugins-setup.lua
@@ -1,13 +1,13 @@
 -- auto install packer if not installed
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer() -- true only if packer is not installed
@@ -29,68 +29,68 @@ if not status then
 end
 
 return packer.startup(function(use)
-	
-	use "wbthomason/packer.nvim"  -- packer can manage itself
-  use "nvim-lua/popup.nvim"  -- An implementation of the Popup API from vim in Neovim. Hope to upstream this eventually.
-  use "nvim-lua/plenary.nvim" -- lua functions that many plugins use
-  use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and lsp
+	use("wbthomason/packer.nvim") -- packer can manage itself
+	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim. Hope to upstream this eventually.
+	use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
+	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and lsp
 
 	-- for creating custom colorscheme
-	use "rktjmp/lush.nvim"
+	use("rktjmp/lush.nvim")
 
-  -- github copilot
-	use "github/copilot.vim"
+	-- github copilot
+	use("github/copilot.vim")
 
-  -- commenting with gc
-	use "numToStr/Comment.nvim"
+	-- commenting with gc
+	use("numToStr/Comment.nvim")
 
-  -- color theme
-  use "~/personal/tropical"
-  use "folke/tokyonight.nvim"
+	-- color theme
+	use("~/personal/tropical")
+	use("folke/tokyonight.nvim")
 
-  -- statusline
-	use "nvim-lualine/lualine.nvim"
+	-- statusline
+	use("nvim-lualine/lualine.nvim")
 
-  -- harpoon
-	use "ThePrimeagen/harpoon" -- quick navigation between files
+	-- harpoon
+	use("ThePrimeagen/harpoon") -- quick navigation between files
 
-  use "christoomey/vim-tmux-navigator" -- tmux & split window navigation
+	use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
+	use("szw/vim-maximizer") -- maximize split window
 
-  use "tpope/vim-surround" -- add, delete, change surroundings (it's awesome)
+	use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
 
-  -- cmp plugins
-  use "hrsh7th/nvim-cmp" -- the completion plugin
-  use "hrsh7th/cmp-buffer" -- buffer completion source for nvim-cmp
-  use "hrsh7th/cmp-path" -- path completion source for nvim-cmp
-  use "hrsh7th/cmp-cmdline" -- cmdline completion source for nvim-cmp
-  use "saadparwaiz1/cmp_luasnip" -- snippets completion source for nvim-cmp
-  use "hrsh7th/cmp-nvim-lsp" -- nvim-lsp completion source for nvim-cmp
-  use "hrsh7th/cmp-nvim-lua" -- nvim-lua completion source for nvim-cmp
+	-- cmp plugins
+	use("hrsh7th/nvim-cmp") -- the completion plugin
+	use("hrsh7th/cmp-buffer") -- buffer completion source for nvim-cmp
+	use("hrsh7th/cmp-path") -- path completion source for nvim-cmp
+	use("hrsh7th/cmp-cmdline") -- cmdline completion source for nvim-cmp
+	use("saadparwaiz1/cmp_luasnip") -- snippets completion source for nvim-cmp
+	use("hrsh7th/cmp-nvim-lsp") -- nvim-lsp completion source for nvim-cmp
+	use("hrsh7th/cmp-nvim-lua") -- nvim-lua completion source for nvim-cmp
 
-  -- snippets
-  use "L3MON4D3/LuaSnip" -- snippet engine
-  use "rafamadriz/friendly-snippets" -- snippets collection
+	-- snippets
+	use("L3MON4D3/LuaSnip") -- snippet engine
+	use("rafamadriz/friendly-snippets") -- snippets collection
 
-  -- LSP
-  use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/mason.nvim" -- simple to use language server installer
-  use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
-  use { "jose-elias-alvarez/null-ls.nvim", branch="0.7-compat" } -- LSP diagnostics and code actions
+	-- LSP
+	use("neovim/nvim-lspconfig") -- enable LSP
+	use("williamboman/mason.nvim") -- simple to use language server installer
+	use("williamboman/mason-lspconfig.nvim") -- simple to use language server installer
+	use({ "jose-elias-alvarez/null-ls.nvim", branch = "0.7-compat" }) -- LSP diagnostics and code actions
 
-  -- Telescope
-  use { "nvim-telescope/telescope.nvim", tag="0.1.0" }
-  use "nvim-telescope/telescope-media-files.nvim"
-  use "JoosepAlviste/nvim-ts-context-commentstring"
+	-- Telescope
+	use({ "nvim-telescope/telescope.nvim", tag = "0.1.0" })
+	use("nvim-telescope/telescope-media-files.nvim")
+	use("JoosepAlviste/nvim-ts-context-commentstring")
 
-  -- Treesitter
-  use { "nvim-treesitter/nvim-treesitter", tag="v0.7.2", run = ":TSUpdate" }
+	-- Treesitter
+	use({ "nvim-treesitter/nvim-treesitter", tag = "v0.7.2", run = ":TSUpdate" })
 
-  -- Git
-  use { "lewis6991/gitsigns.nvim", tag="v0.6" }
+	-- Git
+	use({ "lewis6991/gitsigns.nvim", tag = "v0.6" })
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
-		require('packer').sync()
+		require("packer").sync()
 	end
 end)
