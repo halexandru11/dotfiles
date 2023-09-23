6ebee1cbe189b5b59bd885952b6249423b5e9b0a
halexandru11 <77728536+halexandru11@users.noreply.github.com>
2023-09-23 14:43:54 +0300

    Added hot reload in flutter

--- .config/nvim/lua/user/plugins/lsp/handlers.lua
+++ .config/nvim/lua/user/plugins/lsp/handlers.lua
@@ -64,10 +64,14 @@ end
 
 M.on_attach = function(client, bufnr)
 	if client.name == "tsserver" or client.name == "lua_ls" or client.name == "jsonls" then
-		--[[ client.resolved_capabilities.document_formatting = false ]]
-		--[[ client.resolved_capabilities.document_range_formatting = false ]]
 		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentRangeFormattingProvider = false
 	end
+	if client.name == "dartls" then
+		local hotReload = 'autocmd BufWritePost *.dart :silent ! kill -SIGUSR1 $(pgrep -f "[f]lutter_tool.*run") &'
+		vim.cmd(hotReload)
+	end
+
 	lsp_keymaps(bufnr)
 end
 
