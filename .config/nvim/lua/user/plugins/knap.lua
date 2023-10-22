3ba15b23c1e985dad1ece9345365edf3fd40280c
halexandru11 <77728536+halexandru11@users.noreply.github.com>
2023-10-22 18:24:33 +0300

    update: now pdf updates every time you save the .tex file

--- .config/nvim/lua/user/plugins/knap.lua
+++ .config/nvim/lua/user/plugins/knap.lua
@@ -4,16 +4,36 @@ if not knap_status then
 	return
 end
 
+local function swallow_output(callback)
+	print = function(...) end
+	pcall(callback, arg)
+end
+
+local group = vim.api.nvim_create_augroup("KnapReload", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = "*.tex",
+	group = group,
+	callback = function()
+		swallow_output(function()
+			local _ = knap.process_once()
+		end)
+	end,
+})
+
 local keymap = vim.keymap
 
-keymap.set({ "n", "v", "i" }, "<leader><leader>s", function()
+keymap.set({ "n", "v" }, "<leader><leader>p", function()
+	knap.process_once()
+end)
+
+keymap.set({ "n", "v" }, "<leader><leader>s", function()
 	knap.toggle_autopreviewing()
 end)
 
-keymap.set({ "n", "v", "i" }, "<leader><leader>c", function()
+keymap.set({ "n", "v" }, "<leader><leader>c", function()
 	knap.forward_jump()
 end)
 
-keymap.set({ "n", "v", "i" }, "<leader><leader>e", function()
+keymap.set({ "n", "v" }, "<leader><leader>e", function()
 	knap.close_viewer()
 end)
