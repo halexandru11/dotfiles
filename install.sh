3a6e4083c792860c0c15283166331fe5cd18384c
halexandru11 <77728536+halexandru11@users.noreply.github.com>
2023-12-05 16:01:21 +0200

    improved 'install.sh' script
    changed pwd with a constant
    
    added git repo of powerlevel10k for the zsh prompt

--- install.sh
+++ install.sh
@@ -5,27 +5,37 @@ sudo apt-get update && sudo apt-get upgrade -y
 
 sudo apt install zsh -y ;
 chsh -s $(which zsh) $USER
-echo "Run `zsh` to create zsh files"
-echo "To change the shell run `chsh -s $(which zsh) [username]`"
+echo "Run `zsh` to create zsh files" ;
+echo "To change the shell run `chsh -s $(which zsh) $USER`" ;
 
 sudo apt install git -y ;
 
 #Download Hermit Nerd Font Mono
-curl -fOL --create-dirs --output-dir .local/share/fonts "https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/Hermit/{Bold\-Italic,Bold,Italic,Light\-Italic,Light,Regular}/HurmitNerdFont-{BoldItalic,Bold,Italic,LightItalic,Light,Regular}.otf"
+curl -fOL --create-dirs --output-dir .local/share/fonts "https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/Hermit/{Bold\-Italic,Bold,Italic,Light\-Italic,Light,Regular}/HurmitNerdFont-{BoldItalic,Bold,Italic,LightItalic,Light,Regular}.otf" ;
 
-# Create symbolic links
-ln -s `pwd`/.zshrc ~/.zshrc
-ln -s `pwd`/.config/zsh ~/.config/zsh
-ln -s `pwd`/.config/nvim ~/.config/nvim
-ln -s `pwd`/powerlevel10k ~/powerlevel10k
-ln -s `pwd`/.p10k.zsh ~/.p10k.zsh
-ln -s `pwd`/.gitconfig ~/.gitconfig
+$DOTFILES_DIR=~/.dotfiles
+rm ~/.zshrc ; ln -s -f $DOTFILES_DIR/.config/zsh/.zshrc ~/.zshrc ;
+rm -r ~/.config/zsh ; ln -s -f $DOTFILES_DIR/.config/zsh ~/.config/zsh ;
+rm -r ~/.config/nvim ; ln -s -f $DOTFILES_DIR/.config/nvim ~/.config/nvim ;
+rm -r ~/.p10k.zsh ; ln -s -f $DOTFILES_DIR/.config/zsh/.p10k.zsh ~/.p10k.zsh ;
+rm -r ~/.gitconfig ; ln -s -f $DOTFILES_DIR/.gitconfig ~/.gitconfig ;
 
-rm -rf ~/.local/share/fonts
-ln -s `pwd`/.local/share/fonts ~/.local/share/fonts
-fc-cache -f -v
+rm -rf ~/.local/share/fonts ;
+ln -s $DOTFILES_DIR/.local/share/fonts ~/.local/share/fonts ;
+fc-cache -f -v ;
 
 # Install packages
 sudo apt install -y \
   autojump \
-  cmatrix;
+  build-essential \
+  clang \
+  cmatrix \
+  gnome-shell-extension-manager \
+  libfuse2 \
+  libgtk-3-dev \
+  nmp \
+  nodejs \
+  pkg-config \
+  xclip;
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
