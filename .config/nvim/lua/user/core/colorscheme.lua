b1453a68f11b4a9887b81e62174bfa43a1aeef59
halexandru11 <77728536+halexandru11@users.noreply.github.com>
2024-01-29 20:31:57 +0200

    make terminal background translucent

--- .config/nvim/lua/user/core/colorscheme.lua
+++ .config/nvim/lua/user/core/colorscheme.lua
@@ -1,21 +1,33 @@
 --[[ vim.cmd 'colorscheme tropical' ]]
 
 -- tokyonight
--- local colorscheme = "tokyonight-night"
+-- local colorscheme_name = "tokyonight-night"
+-- local flavor = ""
 
 -- catppuccin
--- local colorscheme = "catppuccin-latte"  --light
--- local colorscheme = "catppuccin-frappe"
--- local colorscheme = "catppuccin-macchiato"
--- local colorscheme = "catppuccin-mocha"
+-- local colorscheme_name = "catppuccin"
+-- local flavour = "-latte"      --light
+-- local flavour = "-frappe"
+-- local flavour = "-macchiato"
+-- local flavour = "-mocha"      -- dark
 
--- local colorscheme = "rose-pine-dawn"  -- light
--- local colorscheme = "rose-pine"
--- local colorscheme = "rose-pine-moon"
-local colorscheme = "rose-pine-main" -- darkest
+-- rose-pine
+local colorscheme_name = "rose-pine"
+-- local flavour = "-dawn"       -- light
+-- local flavour = ""
+-- local flavour = "-moon"
+local flavour = "-main" -- darkest
 
-local status, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status then
-	print("Colorscheme not found")
-	return
+local status_load, colorscheme = pcall(require, colorscheme_name)
+if not status_load then
+	error("Couldn't load colorscheme " .. colorscheme_name)
+end
+
+colorscheme.setup({
+	disable_background = true,
+})
+
+local status_set, _ = pcall(vim.cmd, "colorscheme " .. colorscheme_name .. flavour)
+if not status_set then
+	error("Couldn't set colorscheme " .. colorscheme_name .. flavour)
 end
