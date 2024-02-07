81766cf1090b84aa0917d471f1cc6bfd51234fcf
halexandru11 <77728536+halexandru11@users.noreply.github.com>
2024-02-07 17:10:05 +0200

    add basic tmux config

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ echo "To change the shell run `chsh -s $(which zsh) \$USER`" ;
 
 sudo apt install git -y ;
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm ;
+
 #Download Hermit Nerd Font Mono
 curl -fOL --create-dirs --output-dir ~/.local/share/fonts "https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/Hermit/{Bold\-Italic,Bold,Italic,Light\-Italic,Light,Regular}/HurmitNerdFont-{BoldItalic,Bold,Italic,LightItalic,Light,Regular}.otf" ;
 curl -fOL --create-dirs --output-dir ~/.local/share/fonts "https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/FiraMono/{Bold,Medium,Regular}/{FiraMonoNerdFont,FiraMonoNerdFontMono}-{Bold,Medium,Regular}.otf"
