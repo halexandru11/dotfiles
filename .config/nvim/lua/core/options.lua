526cf6df0ab3b42dbcf098e82d98fb6ecde6f743
halexandru11 <77728536+halexandru11@users.noreply.github.com>
2024-02-13 16:01:34 +0200

    update: remove . and .. from Netrw (file view) in neovim

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -43,3 +43,5 @@ opt.splitbelow = true -- split horizontal window to the bottom
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+vim.g.netrw_list_hide = "^..$,^...$"
+vim.g.netrw_hide = 1
