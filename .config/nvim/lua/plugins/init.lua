f2a0d707a7964dcd8d47c8eab897d31f9dfbe2bf
halexandru11 <77728536+halexandru11@users.noreply.github.com>
2024-04-20 17:00:00 +0300

    add highlighting for special comments (todo, fixme, hack etc)

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -18,5 +18,12 @@ return {
 			require("Comment").setup()
 		end,
 	},
+	-- Highlight todo, notes, etc in comments
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = true },
+	},
 	-- use({ "lervag/vimtex", opt = true, cmd = { "VimtexCompile", "VimtexStopCompile", "Start" } }) -- latex support
 }
